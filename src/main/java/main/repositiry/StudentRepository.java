69321b3fa3c85b6fd87b6f3645eac409ad815623
Moskvichev <beldetal@yandex.ru>
2022-02-14 22:07:50 +0300

    finish

---
+++ src/main/java/main/repositiry/StudentRepository.java
@@ -0,0 +1,12 @@
+package main.repositiry;
+
+import main.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
+
+
